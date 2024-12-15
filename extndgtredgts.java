930efb127b58f23fee3b65945ece62bda20df994
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 11:26:35 +0530

    Add files via upload

---
+++ extndgtredgts.java
@@ -0,0 +1,16 @@
+class a extends Thread{
+    public void r()
+    {
+        System.out.println("hello i'am thread");
+
+    }
+}
+public class extndgtredgts{
+    public static void main(String[] args){
+        a g=new a();
+        g.r();
+        g.start();   
+    }
+}
+/*hello i'am thread
+PS C:\Users\Student.D */
